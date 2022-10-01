6813f937903d857023a1199bbf8b9add8a8cde2c
philippe44 <philippe44@users.noreply.github.com>
2022-10-01 15:02:42 -0700

    os is host

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ do
 		continue
 	fi	
 	
-	IFS=- read -r platform os dummy <<< ${alias[$cc]:-$cc}
+	IFS=- read -r platform host dummy <<< ${alias[$cc]:-$cc}
 
 	make CC=$cc PLATFORM=$platform $clean
 	if [ $clean ]; then
